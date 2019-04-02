99c25a5b98f4d0094cd1b432821ec4b0eb08fcd3
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-02 10:45:33 +0200

    Update templates

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,21 +1,32 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const StyledDiv = styled.div`
-background-color: beige;
+background-color: #222;
 height: 100vh;
 width: 100vw;
+
+body {
+  margin:0;
+}
+
 p {
-  color: goldenrod;
+  color: #999;
   font-size: 25px;
+}
+a {
+  color:white;
+  margin:20px;
 }
 `
 export const TEST_QUERY = gql`
   query GET_POSTS {
-    activities {
+    branches {
       edges {
         node {
+            slug
             title
         }
       }
@@ -27,13 +38,13 @@ const Index = () => (
   <StyledDiv>
       <Query query={TEST_QUERY}>
         {({ data }) => {
-          console.log(data.activities.edges.map(item => {
+          console.log(data.branches.edges.map(item => {
             console.log(item.node.title)
           }))
             return (
               <div>
-                {data.activities.edges.map(item => {
-                  return <p>{item.node.title}</p>
+                {data.branches.edges.map(item => {
+                  return <Link href={ `/branches/${ item.node.slug }` }><a href={ `/branches/${ item.node.slug }` }>{ item.node.title }</a></Link>
                 })}
               </div>
                 );
---
+++ front-end/pages/posts.js
@@ -0,0 +1,7 @@
+const Posts = () => (
+  <div>
+    <h1>posts!!</h1>
+  </div>
+);
+
+export default Posts;
