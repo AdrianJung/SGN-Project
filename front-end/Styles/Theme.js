1a137b71d3066a8f133c1be60d1707bdbcb733eb
Adrian Jungnelius <a.jungnelius@gmail.com>
2019-04-11 15:10:37 +0200

    updated theme and styles

--- front-end/Styles/Theme.js
+++ front-end/Styles/Theme.js
@@ -1,24 +1,18 @@
 const Theme = {
-    // Color
-    colorPrimary: '#225545;',
-    colorDarkPrimary: '#005550',
-    colorWhite: '#fff',
-    colorLightGrey: '#F1F1F2',
-    colorGrey: '#D2D2D2',
-    colorDarkGrey: '#393939',
+  // Color
+  colorPrimary: '',
+  colorDarkPrimary: '',
+  colorWhite: '',
 
-    // Font
-    fontMobileH1: "bold 28px/95% 'Roboto', sans-serif",
-    fontMobileH2: "bold 22px/110% 'Roboto', sans-serif",
-    fontMobileH3: "bold 18px/100% 'Roboto', sans-serif",
-    fontMobileH4: "bold 12px/100% 'Roboto', sans-serif",
-    fontMobilePBold: "bold 16px/120% 'Roboto', sans-serif",
-    fontMobileP: "16px/120% 'Roboto', sans-serif",
+  // Font
+  fontDesktopH1: "bold 64px/100% 'Roboto', sans-serif",
+  fontDesktopH2: "42px/100% 'Libre Franklin Semi-Bold', sans-serif",
+  fontDesktopH3: "bold 24px/130% 'Roboto', sans-serif",
+  fontDesktopIngress: "regular 24px/130% 'Roboto' sans-serif",
+  fontDesktopBodyText: "regular 18px/130% 'Roboto' sans-serif",
 
-    fontMini: "11px/100% 'Roboto', sans-serif",
-    fontDesktopH1: "bold 40px/100% 'Roboto', sans-serif",
-    fontDesktopP: "18px/130% 'Roboto', sans-serif",
-    fontNavItemsDesktop: "14px/100% 'Roboto', sans-serif"
+  fontMobileH1: "24px/100% 'Libre Franklin Semi-Bold', sans-serif",
+  fontNavItemsDesktop: "14px/100% 'Roboto', sans-serif"
 
-  }
-  export default Theme;
+}
+export default Theme;
