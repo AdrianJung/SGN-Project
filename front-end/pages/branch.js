9739156f8483515f5639d8a35b351b2ce5322b55
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-11 11:07:35 +0200

    Add hero styled component

--- front-end/pages/branch.js
+++ front-end/pages/branch.js
@@ -3,14 +3,108 @@ import Layout from '../components/Layout/'
 import React, { Component } from 'react'
 import Head from 'next/head'
 import axios from 'axios'
-const BranchStyle = styled.div`
-display:flex;
-justify-content:flex-end;
+import Link from 'next/link';
 
-h1 {
-  font-family: sans-serif;
-}
+const BranchStyle = styled.div`
 `
+const Hero = styled.div`
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  height:100vh;
+  width:100vw;
+  padding-right:150px;
+  box-sizing:border-box;
+
+  button {
+    z-index:3;
+    margin-right:20px;
+  }
+
+  h1 {
+    font-family: sans-serif;
+    z-index:3;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 64px;
+    line-height: normal;
+    letter-spacing: 0.03em;
+  }
+
+  img {
+    width:100vw;
+    height:100vh;
+    position:absolute;
+  }
+
+  section {
+    z-index:3;
+    margin-left:150px;
+  }
+
+  div {
+    width:100vw;
+    height:100vh;
+    position:absolute;
+    opacity:0.5;
+    z-index:2;
+    background:black;
+  }
+
+  .white {
+    background:white;
+    border-radius:4px;
+    color:black;
+    border:none;
+    padding:15px 20px;
+    font-size:14px;
+  }
+
+  .black {
+    background:none;
+    border:solid white 2px;
+    border-radius:4px;
+    color:white;
+    padding:13px 24px;
+    font-size:14px;
+  }
+
+  @media screen and (max-width: 992px) {
+
+    justify-content:flex-start;
+
+    div {
+      position:static;
+      height:60vh;
+    }
+
+    img {
+      height:60vh;
+    }
+
+    section {
+      display:flex;
+      flex-direction:column;
+      justify-content:space-between;
+      padding: 60px 16px;
+      box-sizing:border-box;
+      width:100vw;
+      margin:0;
+      height:40vh;
+      background:#005874;
+    }
+
+    h1 {
+      margin:0 0 10px 0;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 32px;
+      line-height: normal;
+      letter-spacing: 0.03em;
+    }
+  }
+`
+
 
 class Branch extends Component {
 
@@ -61,8 +155,26 @@ class Branch extends Component {
         `}</style>
         {!this.state.isLoading &&
         <BranchStyle>
-          <img src={this.state.branch.acf.header_image} />
-          <h1>{this.props.slug}</h1>
+
+
+          <Hero>
+            {/*<h1>We have to think of a headline with this length.</h1>
+            <section>
+              <Link href="/events"><button className="white">Upcoming Events</button></Link>
+              <Link href="/contact"><button className="black">Contact Us</button></Link>
+            </section>*/}
+
+            <div></div>
+            <img src={this.state.branch.acf.header_image} />
+            <section>
+              <h1>We have to think of a headline with this length.</h1>
+              <article>
+                <Link href="/events"><button className="white">Upcoming Events</button></Link>
+                <Link href="/contact"><button className="black">Contact Us</button></Link>
+              </article>
+            </section>
+          </Hero>
+
         </BranchStyle>}
       </Layout>
     )
