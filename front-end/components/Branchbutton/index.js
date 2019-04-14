32cb5f1cb5c8ff35d45afc616fdbd3d60cdd4592
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-14 09:48:28 +0200

    Remove console.log

--- front-end/components/Branchbutton/index.js
+++ front-end/components/Branchbutton/index.js
@@ -85,7 +85,7 @@ class BranchButton extends React.Component {
     axios.get('http://localhost:8888/wp-json/wp/v2/branches')
     .then((response) => {
       // handle success
-      console.log(response.data);
+      // console.log(response.data);
       this.setState({branches: response.data})
     })
   }
