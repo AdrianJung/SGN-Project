780c277d226f4ff62d7d345615a22c28932ca44c
hannaell <hannaell91@gmail.com>
2019-04-15 11:44:21 +0200

    Update home page

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,11 +1,30 @@
 import Link from 'next/link';
 import Head from 'next/head'
 import Layout from '../components/Layout/'
+import Hero from '../components/Hero/';
+import WhoAreWeCard from '../components/WhoAreWeCard/';
+import WhatDoWeDoCard from '../components/WhatDoWeDoCard/';
+import WorkWithUsCard from '../components/WorkWithUsCard/';
+import FacebookCard from '../components/FacebookCrad/';
+import VideoCard from '../components/VideoCard/'
+import ActivityCard from '../components/ActivityCard/'
+import StoryCard from '../components/StoryCard/'
 
 const Index = () => (
   <Layout>
     <Head>
     </Head>
+    <Hero/>
+    <WhoAreWeCard/>
+    <WhatDoWeDoCard/>
+    <WorkWithUsCard/>
+    <FacebookCard/>
+    <VideoCard
+      url = "https://player.vimeo.com/video/316874134"
+    />
+    {/* <ActivityCard/> */}
+    <StoryCard/>
+    <WorkWithUsCard/>
   </Layout>
 );
 
