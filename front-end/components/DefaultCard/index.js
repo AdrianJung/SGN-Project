820d31f13667aadabfe8316c41a50eadb0c09026
Adrian Jungnelius <a.jungnelius@gmail.com>
2019-04-19 14:12:44 +0200

    changed padding for mobile

--- front-end/components/DefaultCard/index.js
+++ front-end/components/DefaultCard/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import styled, { css } from "styled-components";
 
 const StyledCard = styled.div`
-@media screen and (max-width: 992px) {
-  padding: 95px 10px;
-}
+  @media screen and (max-width: 992px) {
+    padding: 95px 10px;
+  }
   margin: 64px 0;
   padding: 95px 100px;
   display: flex;
@@ -12,20 +12,14 @@ const StyledCard = styled.div`
   height: 420px;
   background: white;
   box-sizing: border-box;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
 
-  ${props =>
-    props.scroll &&
-    css`
-      overflow-x: scroll;
-      -webkit-overflow-scrolling: touch;
-      overflow-y: hidden;
-      white-space: nowrap;
-    `}
-  flex-direction: ${props => (props.isColumn ? "column" : "row")};
-  align-items: ${props => (props.isColumn ? "flex-start" : "center")};
-  justify-content: ${props => (props.isColumn ? "space-between" : "none")};
   box-shadow: 0px 1px 20px rgba(0, 0, 0, 0.08);
-
+  @media screen and (max-width: 992px) {
+    padding: 95px 30px;
+  }
   p {
     font: ${props => props.theme.fontDesktopBodyText};
   }
