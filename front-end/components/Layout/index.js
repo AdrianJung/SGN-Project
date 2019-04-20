c2968ee20309c7b033e07afd7531865263efbc87
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-20 12:14:45 +0200

    Use favicon package

--- front-end/components/Layout/index.js
+++ front-end/components/Layout/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import Header from "../Header/";
 import Footer from "../Footer/";
 import GlobalStyle from "../../Styles/GlobalStyle";
+import Head from 'next/head'
+import Favicon from 'react-favicon';
 
 const Layout = props => {
   return (
     <div>
+      <Head>
+      </Head>
+      <Favicon url="https://i.imgur.com/07qLFa0.png" />
       <Header title={props.title} />
       <GlobalStyle />
       {props.children}
