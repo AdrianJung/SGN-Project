fb9d6e37457083cb7c625fdc5ede019c81187be1
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-23 11:13:35 +0200

    Update background styling

--- front-end/components/Branchbutton/index.js
+++ front-end/components/Branchbutton/index.js
@@ -44,17 +44,20 @@ const MenuDiv = styled.div`
   top:0;
   overflow-y:scroll;
   display:flex;
-  padding:85px 0 0 0;
+  margin-top:84px;
   flex-direction:column;
   z-index:-1;
   height:100vh;
+  background-color:rgba(0,0,0,0.5);
 
   @media screen and (min-width: 992px) {
     padding:53px 0 0 0;
     width:236px;
     height:100vh;
-    background-color:rgba(224, 224, 224, 0.98);
+    background-color:white;
     position:fixed;
+    margin:0;
+    border-right:1px solid rgba(0,0,0,0.1);
   }
 `
 
@@ -74,6 +77,16 @@ const BranchLink = styled.a`
   align-items:center;
   padding:0 0 0 25px;
   cursor:pointer;
+  position:relative;
+
+  .pre {
+    position:absolute;
+    width:70%;
+    height:1px;
+    background:lightgrey;
+    top:0;
+    left:25px;
+  }
 
   svg, img {
     margin:0 15px 0 0;
@@ -123,6 +136,16 @@ class BranchButton extends React.Component {
       }
     }
 
+    const colors = [
+      '#AD0039',
+      '#630079',
+      '#005B91',
+      '#256600',
+      '#9C3600',
+      '#685348',
+      '#4A5389'
+    ]
+
     return (
       <div>
         <ContainerDiv onClick={this.props.onClick ? this.props.onClick:
@@ -149,8 +172,8 @@ class BranchButton extends React.Component {
                   as={`/branches/${item.slug}`}>
 
                   <BranchLink
-                    style={{...{backgroundColor: key % 2 == 0 ? 'rgba(238, 238, 238, 0.97)' : 'rgba(224, 224, 224, 0.98)'}}}>
-                    <svg width="16" height="16" xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 512 512'>
+                    style={{...{backgroundColor: this.props.title == item.acf.name ? 'rgba(238, 238, 238, 0.97)' : 'white'}}}>
+                    <svg fill={colors[key]} width="16" height="16" xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 512 512'>
                         <path d='M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035 c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719 c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z'
                         />
                     </svg>
@@ -159,7 +182,7 @@ class BranchButton extends React.Component {
                 </Link>
               )
             })}
-            <BranchLink href="https://instagram.com">Instagram</BranchLink>
+            <BranchLink href="https://instagram.com"><div className="pre"></div>Instagram</BranchLink>
             <BranchLink href="https://facebook.com">Facebook</BranchLink>
             <Link href='/contact'><BranchLink>Contact us</BranchLink></Link>
           </BranchesDiv>
