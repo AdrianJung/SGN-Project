d052d14e59f8e0f6be39eda8b805f0f11b8a35f4
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-24 10:57:52 +0200

    Update alignment

--- front-end/components/ActivitiesCard/index.js
+++ front-end/components/ActivitiesCard/index.js
@@ -33,7 +33,7 @@ const Activities = styled.div`
         display: flex;
         flex-wrap: wrap;
         justify-content: space-evenly;
-        align-items: center;
+        align-items: flex-start;
     }
 `
 
